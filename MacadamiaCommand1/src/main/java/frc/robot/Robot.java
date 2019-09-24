3774cba57edfbfbb49117294569f11cc70fb83e0
ewang4255 <ewang3562@gmail.com>
2019-09-24 19:29:30 -0400

    new

--- MacadamiaCommand1/src/main/java/frc/robot/Robot.java
+++ MacadamiaCommand1/src/main/java/frc/robot/Robot.java
@@ -8,90 +8,78 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import frc.robot.drive.DriveCommand;
+import frc.robot.drive.DriveSubsystem;
+import edu.wpi.first.cameraserver.CameraServer;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  public static DriveSubsystem driveSys;
+  double leftEncoderStartValue, rightEncoderStartValue;
+  private final double safeDistance = 30.0;
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
+  // Use this function for all initialization code
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    
+    driveSys = new DriveSubsystem();
+    CameraServer.getInstance().startAutomaticCapture();
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
+  // Called periodically regardless of the game period
   @Override
   public void robotPeriodic() {
+    Scheduler.getInstance().run();
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
+
+  /* Sandstorm Period */
+  // Called at the beginning of the Sandstorm
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
+  // Called periodically during the Sandstorm
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    double distance = Robot.driveSys.getFrontUltrasonic();
+
+    if (distance < safeDistance) {
+      Robot.driveSys.drive.tankDrive(-0.33, -0.33);
+    } else if (distance > safeDistance) {
+      Robot.driveSys.drive.tankDrive(0.33, 0.33);
+    } else {
+      Robot.driveSys.drive.tankDrive(0.0, 0.0);
     }
+
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
+
+  /* Teleop Period */
+  // Called at the beginning of the Teleop period
+  @Override
+  public void teleopInit() {
+  }
+  
+  // Called periodically during the Teleop period
   @Override
   public void teleopPeriodic() {
+    Scheduler.getInstance().removeAll();
+    Scheduler.getInstance().add(new DriveCommand());
+    Scheduler.getInstance().run();
+
+
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
+
+
+  @Override
+  public void testInit() {
+    
+  }
   @Override
   public void testPeriodic() {
+
   }
 }
